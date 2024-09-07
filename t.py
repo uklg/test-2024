e20ae2e8a6c38a2e7958c3933a1dae4d9c297e25
murdochr <rmu@live.co.uk>
2024-09-07 11:07:47 +0100

    reformat

--- t.py
+++ t.py
@@ -1,4 +1,5 @@
 import pyperclip
+
 # needs `sudo apt-get install xclip
 
 print('this is a test')
@@ -6,6 +7,13 @@ print('this is a test')
 #pyperclip.copy('I love you babe')
 
 
-
-
 print(pyperclip.paste())
+
+for x in range(100):
+    print(x)
+
+print(1)
+
+
+def dog():
+    print(1)
